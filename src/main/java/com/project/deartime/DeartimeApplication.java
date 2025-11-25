18111e9ec1713372e68a8cfd287928de205bf51f
yoonjae-kwon <yoonjae.kwon03@gmail.com>
2025-11-25 19:29:20 +0900

    레포지터리 기본 세팅

---
+++ src/main/java/com/project/deartime/DeartimeApplication.java
@@ -0,0 +1,13 @@
+package com.project.deartime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeartimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeartimeApplication.class, args);
+	}
+
+}
