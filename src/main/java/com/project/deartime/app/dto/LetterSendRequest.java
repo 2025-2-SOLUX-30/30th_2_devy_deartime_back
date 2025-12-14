a2a0ecb3ef4916a82b1383e69f77be43ec99287d
dmseong <149465698+dmseong@users.noreply.github.com>
2025-12-15 03:54:28 +0900

    chore: remove unnecessary annotation

--- src/main/java/com/project/deartime/app/dto/LetterSendRequest.java
+++ src/main/java/com/project/deartime/app/dto/LetterSendRequest.java
@@ -1,7 +1,6 @@
 package com.project.deartime.app.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDateTime;
